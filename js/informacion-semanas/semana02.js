a6178ee7c2dd6911362ee297afe0c7bfeaa43244
Edgar <egagouribe15@gmail.com>
2026-09-15 21:28:59 -0500

    tema semana 2

--- js/informacion-semanas/semana02.js
+++ js/informacion-semanas/semana02.js
@@ -1,5 +1,89 @@
 window.cuadernoWeeksData = window.cuadernoWeeksData || {};
 cuadernoWeeksData[2] = {
-  left: [],
+  left: [
+    {
+      chip: 'Tema',
+      title: 'Diseñar una Página Web con Hojas de Estilos',
+    },
+    {
+      subtitle: 'Definiciones',
+    },
+    {
+      text: '<b>Estructura HTML</b> <br> El HTML permite presentar el contenido de una página web a través del uso de etiquetas nidadas entre sí, formando a su vez una jerarquía.',
+    },
+    {
+      text: '<b>Estructura XML</b> <br> El XML es también un lenguaje de marcado pero centrado en la funcionalidad de transportar datos, permitiendo que diferentes programas puedan compartir información.',
+    },
+    {
+      text: '<b>Etiquetas de HTML5</b> <br> La versión 5 de HTML incorporó las etiquetas header, nav, main, section, article, aside y footer. Estos permiten describir partes del HTML por sí mismos para no tener que usar varios divs.',
+    },
+    {
+      text: '<b>Árbol DOM</b> <br> Es la representación en memoria de un HTML en forma de estructura de árbol compuesto por nodos de elementos, atributos y texto. Su funcionalidad radica en que el navegador pueda modificar el contenido del HTML.',
+    },
+    {
+      text: '<b>Renderizado de Árbol</b> <br> Proceso donde el navegador usa el árbol DOM y el árbol CSSOM para determinar los elementos que serán presentados en pantalla, tanto su posición como apariencia.',
+    },
+    {
+      text: '<b>Ciclo de Vida de una Página Web</b> <br> Son las etapas por las que el navegador solicita el HTML, construye el DOM, descarga el CSS y JS, hasta presentarlo en pantalla para que el usuario pueda interactuar con la página web.',
+    },
+    {
+      text: '<b>Optimización en Motores de Búsqueda</b> <br> Conjunto de métodos que permiten mejorar el posicionamiento de la página web a nivel de resultados de búsqueda con un mejor etiqueta del contenido del HTML.',
+    },
+    {
+      text: '<b>Compatibilidad entre Navegadores</b> <br> Capacidad de una Página Web para poder ser presentada correctamente en diferentes navegadores, sea Chrome, Firefox, Safari, Edge, Brave, Opera, etc.',
+    },
+    {
+      text: '<b>CSS3</b> <br> Es la versión extendida del estándar de Hojas de Estilo en Cascada, el cual incorporó elementos de animaciones, transiciones, flexbox, grid y variables. Gracias a esta versión se pueden diseñar páginas web con caracter responsivo.',
+    },
+    {
+      text: '<b>Modelo de Caja</b> <br> Block ocupa todo el ancho y comienza en una nueva línea. Inline ocupa el espacio de su contenido sin romper línea. Inline-block actúa como inline con la diferencia que permite definir tanto ancho como alto.',
+    },
+    {
+      text: '<b>Unidades de Medida</b> <br> Las unidades absolutas no cambian según los elementos anteriores, mientras que las unidades relativas se calculan en base al elemento padre. Resulta importante conocer ambas unidades para poder diseñar páginas responsive correctamente.',
+      table: {
+        headers: ['Tipo', 'Unidades', 'Uso'],
+        rows: [
+          ['Absolutas', 'px', 'Medidas que no varían por contenedor'],
+          ['Relativas', '%, em, rem, vw, vh', 'Se adaptan al tamaño del padre'],
+        ],
+        caption: 'Diferencia entre Unidades Absolutas y Relativas',
+      },
+    },
+    {
+      text: '<b>Flexbox</b> <br> Es un modelo en el estándar CSS3 que permite organizar los elementos ya sea en filas o columnas, proporcionando las funcionalidades de alineación, distribución y el orden de elementos en un contenedor flexible.',
+    },
+    {
+      text: '<b>Grid Layout</b> <br> Es un modelo en el estándar CSS3 que permite organizar los elementos en filas y columnas al mismo tiempo, como si fuera estructura de cuadrícula.',
+    },
+    {
+      text: '<b>Position</b> <br> Define cómo se ubica un elemento.',
+      table: {
+        headers: ['Valor', 'Función'],
+        rows: [
+          ['static', 'Sigue la ubicación del documento'],
+          ['relative', 'Se desplaza respecto a la posición original'],
+          ['absolute', 'Se ubica respecto al contenedor más cercano'],
+          ['fixed', 'Se ubica respecto a la ventana del navegador'],
+          ['sticky', 'Emplea relative y fixed'],
+        ],
+        caption: 'Position en CSS3',
+      },
+    },
+    {
+      text: '<b>Diseño Fluido</b> <br> Es una forma de maquetación en donde los elementos del documento se presentan con unidades relativas, garantizando así que el contenido se adapte a cualquier tamaño de dispositivo.',
+    },
+    {
+      subtitle: 'Procedimiento',
+    },
+    {
+      text: '1. Desarrollo de clases en hora de teoría <br> 2. Desarrollo de clases en hora práctica <br> 3. Realización de la práctica calificada <br> 4. Realización de la práctica de laboratorio <br> 5. Actualización del cuaderno de la asignatura',
+      image: {
+        src: 'assets/images/Semana02/Semana02Teoria.jpeg',
+        alt: 'Desarrollo de clases teóricas',
+        side: 'right',
+        tilt: -6,
+      },
+    },
+  ],
   right: [],
 };
