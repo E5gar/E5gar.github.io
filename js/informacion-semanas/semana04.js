d4bebf46d83e0db5df5cae77d64e879afa064dad
Edgar <egagouribe15@gmail.com>
2026-09-24 16:59:50 -0500

    tema semana 4

--- js/informacion-semanas/semana04.js
+++ js/informacion-semanas/semana04.js
@@ -1,5 +1,10 @@
 window.cuadernoWeeksData = window.cuadernoWeeksData || {};
 cuadernoWeeksData[4] = {
-  left: [],
+  left: [
+    {
+      chip: 'Tema',
+      title: 'Desarrollar una Página Web Frontend con JavaScript y TypeScript',
+    },
+  ],
   right: [],
 };
